ac5471200bc8a1460f0845641bced64283180af6
Shakir Yakubov <yakuboff.sh@yandex.ru>
2026-03-05 02:00:10 +0300

    initial version without mobile adoptation

---
+++ src/server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+// Статика из src/public
+app.use(express.static(path.join(__dirname)));
+
+// Страницы из src/pages
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "pages", "home.html"));
+});
+// Страницы из src/pages
+app.get("/projects", (req, res) => {
+  res.sendFile(path.join(__dirname, "pages", "projects.html"));
+});
+app.get("/writing", (req, res) => {
+  res.sendFile(path.join(__dirname, "pages", "writing.html"));
+});
+app.get("/photography", (req, res) => {
+  res.sendFile(path.join(__dirname, "pages", "photography.html"));
+});
+app.get("/videography", (req, res) => {
+  res.sendFile(path.join(__dirname, "pages", "videography.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "pages", "about.html"));
+});
+// app.get("/about", (req, res) => {
+//   res.sendFile(path.join(__dirname, "src", "pages", "about.html"));
+// });
+
+// app.get("/portfolio", (req, res) => {
+//   res.sendFile(path.join(__dirname, "src", "pages", "portfolio.html"));
+// });
+
+// // 404
+
+// app.use((req, res) => {
+//   res.status(404).sendFile(path.join(__dirname, "pages", "404.html"));
+// });
+
+app.listen(3000, () => {
+  console.log("🚀 http://localhost:3000");
+});
